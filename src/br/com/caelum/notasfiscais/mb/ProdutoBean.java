5f9b519f23f15af59bbf3e90d6529a59eb614135
tensoja <tensoja@gmail.com>
2013-10-14 15:13:52 -0300

    adicionado metodo de listagem de produtos

--- src/br/com/caelum/notasfiscais/mb/ProdutoBean.java
+++ src/br/com/caelum/notasfiscais/mb/ProdutoBean.java
@@ -1,13 +1,17 @@
 package br.com.caelum.notasfiscais.mb;
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
 
 import br.com.caelum.notasfiscais.dao.ProdutoDao;
 import br.com.caelum.notasfiscais.modelo.Produto;
 
+
 @ManagedBean
 public class ProdutoBean {
 	
 	private Produto produto = new Produto();
+	private List<Produto> produtos;
 	
 	public Produto getProduto(){
 		return this.produto;
@@ -18,6 +22,15 @@ public class ProdutoBean {
 		ProdutoDao dao = new ProdutoDao();
 		dao.adiciona(produto);
 		this.produto = new Produto();
+		
+	}
+	public List<Produto> getProdutos(){
+		if(produtos == null){
+			System.out.println("Carregando produtos...");
+			produtos = new ProdutoDao().listaTodos();
+		}
+		
+		return produtos;
 		
 	}
 }
